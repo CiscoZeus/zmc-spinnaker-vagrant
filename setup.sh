4fcc9e36b56d92e0da932e9543986e4f89a8c8ff
Seanna Vien <smvien@ucdavis.edu>
2017-09-01 15:11:54 -0700

    first commit, Vagrantfile (you need to setup your AWS credentials) and then run the setup.sh in the vagrant

---
+++ setup.sh
@@ -0,0 +1,24 @@
+sudo chmod +x /home/vagrant/InstallHalyard.sh
+sudo bash /home/vagrant/InstallHalyard.sh
+hal config version edit --version "1.1.2"
+
+hal config storage s3 edit \
+        --access-key-id $AWS_ACCESS_KEY_ID \
+        --secret-access-key \
+        --region $AWS_REGION \
+        --bucket $AWS_BUCKET
+
+hal config storage edit --type s3
+
+hal config provider aws edit --access-key-id $AWS_ACCESS_KEY_ID \
+	--secret-access-key
+
+hal config provider aws account add $AWS_ACCOUNT --account-id $AWS_ACCOUNT_ID --assume-role $AWS_ROLE
+
+hal config provider aws enable
+
+
+hal config deploy edit --type localdebian
+
+sudo hal deploy apply
+sudo hal deploy connect
