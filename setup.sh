b541cffd25727f4e7056afecde465d0ed21ef473
snavien <snavien@users.noreply.github.com>
2017-09-01 16:33:46 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,11 @@ sudo chmod +x /InstallHalyard.sh
 sudo bash /InstallHalyard.sh
 hal config version edit --version "1.1.2"
 
-hal config storage s3 edit \
-        --access-key-id $AWS_ACCESS_KEY_ID \
-        --secret-access-key \
-        --region $AWS_REGION \
-        --bucket $AWS_BUCKET
+hal config storage s3 edit --access-key-id $AWS_ACCESS_KEY_ID --secret-access-key --region $AWS_REGION --bucket $AWS_BUCKET
 
 hal config storage edit --type s3
 
-hal config provider aws edit --access-key-id $AWS_ACCESS_KEY_ID \
-	--secret-access-key
+hal config provider aws edit --access-key-id $AWS_ACCESS_KEY_ID --secret-access-key
 
 hal config provider aws account add $AWS_ACCOUNT --account-id $AWS_ACCOUNT_ID --assume-role $AWS_ROLE
 
