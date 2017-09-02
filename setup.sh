9a423dc080c2ca45674e3a5b50da29ff2fa87dc9
Seanna Vien <smvien@ucdavis.edu>
2017-09-01 17:15:54 -0700

    adding the secret key no longer need prompt

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 sudo chmod +x /InstallHalyard.sh
-sudo bash /InstallHalyard.sh
+echo "Y" | sudo bash /InstallHalyard.sh --user $USER
+
 hal config version edit --version "1.1.2"
 
-hal config storage s3 edit --access-key-id $AWS_ACCESS_KEY_ID --secret-access-key --region $AWS_REGION --bucket $AWS_BUCKET
+echo $AWS_SECRET_ACCESS_KEY | hal config storage s3 edit --access-key-id $AWS_ACCESS_KEY_ID --secret-access-key --region $AWS_REGION --bucket $AWS_BUCKET
 
 hal config storage edit --type s3
 
-hal config provider aws edit --access-key-id $AWS_ACCESS_KEY_ID --secret-access-key
+echo $AWS_SECRET_ACCESS_KEY | hal config provider aws edit --access-key-id $AWS_ACCESS_KEY_ID --secret-access-key
 
 hal config provider aws account add $AWS_ACCOUNT --account-id $AWS_ACCOUNT_ID --assume-role $AWS_ROLE
 
