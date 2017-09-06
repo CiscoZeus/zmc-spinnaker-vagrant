7b5ff2aa8608bfacbc87db89f3cbe9cced99609c
svien <svien@cisco.com>
2017-09-06 11:48:10 -0700

    editing hosts and adding aws

--- Vagrantfile
+++ Vagrantfile
@@ -64,8 +64,8 @@ Vagrant.configure("2") do |config|
     # vb.gui = true
 
   #   # Customize the amount of memory on the VM:
-    vb.memory = "4028"
-    vb.cpus = 2
+    vb.memory = "8056"
+    vb.cpus = 4
   end
   #
   # View the documentation for the provider you are using for more
--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
+sudo mkdir ~/.aws
+sudo cp /vagrant/credentials ~/.aws/credentials
 sudo chmod +x /InstallHalyard.sh
+
+sudo sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/hosts
+
+
 echo "Y" | sudo bash /InstallHalyard.sh --user $USER && \
 sleep 30 && \
 hal config version edit --version "1.1.2" && \
@@ -16,15 +22,15 @@ hal config provider aws enable
 
 hal config deploy edit --type localdebian
 
-sudo "host: 0.0.0.0" | tee \
-    ~/.hal/default/service-settings/gate.yml \
-    ~/.hal/default/service-settings/deck.yml
+#sudo "host: 0.0.0.0" | tee \
+#    ~/.hal/default/service-settings/gate.yml \
+#    ~/.hal/default/service-settings/deck.yml
 
-hal config security ui edit \
-    --override-base-url http://192.168.33.10:9000
+#hal config security ui edit \
+#    --override-base-url http://192.168.33.10:9000
 
-hal config security api edit \
-    --override-base-url http://192.168.33.10:8084
+#hal config security api edit \
+#    --override-base-url http://192.168.33.10:8084
 
 sudo hal deploy apply
 sudo hal deploy connect
